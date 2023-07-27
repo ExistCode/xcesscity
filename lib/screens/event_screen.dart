e95691290eb4886249d893fcd0a4755bab7be96c
sexxyrexxy <rexlim2003@gmail.com>
2023-07-27 16:00:32 +0800

    navbar

---
+++ lib/screens/event_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EventScreen extends StatefulWidget {
+  static const routeName = '/event';
+  @override
+  State<EventScreen> createState() => _EventScreenState();
+}
+
+class _EventScreenState extends State<EventScreen> {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+
+      body:Container(
+        width: double.infinity,
+        height:double.infinity,
+      )
+    );
+  }
+}
