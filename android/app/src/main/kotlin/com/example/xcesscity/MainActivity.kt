3c4e91d9f58f74f2307f04b0081852f10953a6fe
Hans Andreanto <andreantohanss@gmail.com>
2023-07-27 14:20:17 +0800

    XcessCity

---
+++ android/app/src/main/kotlin/com/example/xcesscity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xcesscity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryOrange = Color(0xFFFB734C);
+Color whites = Color(0xFFFBFBFB);
+Color black = Color(0xFF232323);
+Color accentOrange = Color(0xFFD04A25);
+Color secondary = Color(0xFF1B998B);
+Color backgroundBlack = Color(0xFF1A172F);
