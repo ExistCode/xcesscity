a0eacd6e035838d58da0d06542a65aff883b6537
sexxyrexxy <rexlim2003@gmail.com>
2023-07-27 14:35:30 +0800

    siu

--- lib/models/colors.dart
+++ lib/models/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color primaryOrange = Color(0xFFFB734C);
-Color whites = Color(0xFFFBFBFB);
+Color white = Color(0xFFFBFBFB);
 Color black = Color(0xFF232323);
 Color accentOrange = Color(0xFFD04A25);
 Color secondary = Color(0xFF1B998B);
