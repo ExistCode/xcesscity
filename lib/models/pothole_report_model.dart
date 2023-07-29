0a01168a6c8deb47dec10620608291e4a873d46d
Hans Andreanto <andreantohanss@gmail.com>
2023-07-29 19:59:00 +0800

    models

---
+++ lib/models/pothole_report_model.dart
@@ -0,0 +1,10 @@
+class PotholeReportModel {
+  String id;
+  DateTime potholeReportDate;
+  String content;
+
+  PotholeReportModel(
+      {required this.id,
+      required this.potholeReportDate,
+      required this.content});
+}
