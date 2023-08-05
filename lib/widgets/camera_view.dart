33dfe1b541b60877865a00b0b5364cbdb1f8bb0d
Hans Andreanto <andreantohanss@gmail.com>
2023-08-05 19:20:19 +0800

    live-detection

--- lib/widgets/camera_view.dart
+++ lib/widgets/camera_view.dart
@@ -2,42 +2,36 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:xcesscity/widgets/scan_controller.dart';
+import 'package:xcesscity/models/colors.dart' as custom_colors;
 
 class CameraView extends StatelessWidget {
   const CameraView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool _isPothole;
+
     return Scaffold(
       body: GetBuilder<ScanController>(
           init: ScanController(),
           builder: (controller) {
             return controller.isCameraInitialized.value
-                ? Stack(
-                    children: [
-                      CameraPreview(controller.cameraController),
-                      Positioned(
-                        // top: (controller.y) * 100,
-                        // right: (controller.x) * 100,
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border:
-                                  Border.all(color: Colors.green, width: 4.0)),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Container(
-                                  decoration:
-                                      BoxDecoration(color: Colors.white),
-                                  child: Text("${controller.label}"))
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
+                ? Container(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top + 20),
+                    child: Stack(
+                      children: [
+                        CameraPreview(controller.cameraController),
+                        Container(
+                            width: 200,
+                            height: 60,
+                            decoration: BoxDecoration(
+                                color: controller.label == "Potholes"
+                                    ? custom_colors.accentOrange
+                                    : custom_colors.white),
+                            child: Center(child: Text("${controller.label}")))
+                      ],
+                    ),
                   )
                 : Center(
                     child: Text("Loading Preview"),
