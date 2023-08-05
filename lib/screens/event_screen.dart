6ccf00ac8c3e00ac5c14c8eb66a4cf99660a5012
sexxyrexxy <rexlim2003@gmail.com>
2023-08-05 16:19:32 +0800

    fixed

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:xcesscity/screens/community_detail_screen.dart';
 import 'package:xcesscity/widgets/community_card.dart';
 import 'package:xcesscity/widgets/crime_map.dart';
@@ -18,190 +19,82 @@ class _EventScreenState extends State<EventScreen> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Stack(
-        children: [
-          Container(
-            alignment: Alignment.topCenter,
-            width: double.infinity,
-            height: 170,
-            decoration: BoxDecoration(
-                color: primaryOrange, borderRadius: BorderRadius.circular(20)),
-          ),
-          Container(
-            padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                bottom: 60,
-                top: MediaQuery.of(context).padding.top + 20),
-            width: double.infinity,
-            child: Column(
+      child: Container(
+        padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: 60,
+            top: MediaQuery.of(context).padding.top + 20),
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              height: 100,
+              padding:EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: primaryOrange,
+                  borderRadius: BorderRadius.circular(20)),
+                  child:Row(children: [
+              Text("POTHOLE HUNTER",
+                  style: TextStyle(
+                      fontSize: 20, color: white, fontWeight: FontWeight.bold)),
+              const SizedBox(width: 10),
+              Icon(Icons.location_pin, color: white, size: 20)
+            ]),
+            ),
+            
+            const SizedBox(height: 20),                                              
+            Text("You have reported 520 potholes",
+                style: TextStyle(
+                    color: white, fontWeight: FontWeight.bold, fontSize: 20)),
+            const SizedBox(height: 20),
+            Container(
+                //For the map, with markers
+                width: double.infinity,
+                height: 260,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  // border:Border.all(width: 1)
+                ),
+                child: MapSample()),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
               children: [
-                Row(children: [
-                  Text("POTHOLE HUNTER",
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: white,
-                          fontWeight: FontWeight.bold)),
-                  const SizedBox(width: 10),
-                  Icon(Icons.location_pin, color: white, size: 20)
-                ]),
-                const SizedBox(height: 20),
-                Container(
-                  width: double.infinity,
-                  height: 137,
-                  padding: const EdgeInsets.only(
-                      top: 10, left: 10, right: 10, bottom: 0),
-                  decoration: BoxDecoration(
-                      color: black.withOpacity(0.8), borderRadius: BorderRadius.circular(15)),
-                  child: Column(
-                    children: [
-                      Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            "Current Points: ",
-                            style: TextStyle(
-                                color: primaryOrange,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      const SizedBox(height: 10),
-                      Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            "1314 pts",
-                            style: TextStyle(
-                                color: primaryOrange,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      const SizedBox(height: 8),
-                      Align(
-                          alignment: Alignment.bottomRight,
-                          child: Text(
-                            "Click to view your rewards",
-                            style: TextStyle(
-                                color: primaryOrange,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                          )),
-                    ],
-                  ),
+                Icon(Icons.brightness_5_outlined, color: secondary, size: 20),
+                Text(
+                  " : Existing Pothole",
+                  style: TextStyle(
+                      color: white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal),
                 ),
-                const SizedBox(height: 15),
-                Text("You have fixed 520 potholes",
-                    style: TextStyle(
-                        color: white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20)),
-                const SizedBox(height: 20),
-                Container(
-                    //For the map, with markers
-                    width: double.infinity,
-                    height: 260,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    child: MapSample()),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    
-                    Icon(Icons.brightness_5_outlined,
-                        color: secondary, size: 20),
-                    Text(
-                      " : Existing Pothole",
-                      style: TextStyle(
-                          color: white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal),
-                    ),
-                    Spacer(),
-                    Icon(Icons.home, color: primaryOrange, size: 20),
-                    Text(
-                      " : Material Supplier",
-                      style: TextStyle(
-                          color: white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    
-                    Icon(Icons.location_pin, color: white, size: 20),
-                    Text(
-                      " : Current Location",
-                      style: TextStyle(
-                          color: white,
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 5),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Select an Existing Pothole: ",
-                    style: TextStyle(
-                        color: white,
-                        fontSize: 13,
-                        fontWeight: FontWeight.normal),
-                  ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 30,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: primaryOrange,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Text(
-                    "POTHOLE FILLED",
-                    style: TextStyle(
-                        color: white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Select an Material Supplier: ",
-                    style: TextStyle(
-                        color: white,
-                        fontSize: 13,
-                        fontWeight: FontWeight.normal),
-                  ),
-                ),
-                SizedBox(height: 5),
-                Container(
-                  width: double.infinity,
-                  height: 30,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      color: primaryOrange,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Text(
-                    "REQUEST MATERIAL",
-                    style: TextStyle(
-                        color: white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
+                Spacer(),
+                Icon(Icons.location_pin, color: white, size: 20),
+                Text(
+                  " : Current Location",
+                  style: TextStyle(
+                      color: white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal),
                 ),
               ],
-            ),
-          ),
-        ],
+            ),            
+            const SizedBox(height: 50),
+            
+            new CircularPercentIndicator(
+                  radius: 60.0,
+                  lineWidth: 10.0,
+                  percent: 237.0/520,                
+                  progressColor: accentOrange,
+                ),
+                SizedBox(height: 20,),
+            Text("237 / 520 Potholes Has Been fixed",style: TextStyle(fontWeight: FontWeight.bold,fontSize:20,color:white),),
+          ],
+        ),
       ),
     );
   }
